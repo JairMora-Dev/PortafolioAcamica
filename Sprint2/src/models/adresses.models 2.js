89b495525d16072bee50ba68b455fe33fcf5f90c
jairMora007 <jairmora07@gmail.com>
2021-09-09 09:43:16 -0500

    Act Readme

--- Sprint2/src/models/adresses.models 2.js
+++
@@ -1,9 +0,0 @@
-module.exports = ( sequelize, DataTypes ) =>{
-    const Address = sequelize.define('addresses', {
-       place:{
-           type: DataTypes.STRING,
-           allowNull: false
-       }
-    });
-    return Address
-};
